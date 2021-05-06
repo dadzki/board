4bbebb9ca48bc8c15c04eb10f26dfe9ec159ded8
Дмитрий Дацковский <dadzki@yandex.ru>
2021-05-06 17:35:06 +0400

    verify command

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Entity\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+            ->count(5)
+            ->create();
+    }
+}
