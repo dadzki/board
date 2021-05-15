3db7f50cb23db202b7b8c3fa50c6803b71303f99
Дмитрий Дацковский <dadzki@yandex.ru>
2021-05-15 09:57:24 +0400

    attributes

--- app/Entity/Adverts/Category.php
+++ app/Entity/Adverts/Category.php
@@ -15,6 +15,7 @@ use Kalnoy\Nestedset\NodeTrait;
  * @property int $depth
  * @property Category $parent
  * @property Category[] $children
+ * @property Attribute[] $attributes
  */
 class Category extends Model
 {
@@ -25,4 +26,20 @@ class Category extends Model
     public $timestamps = false;
 
     protected $fillable = ['name', 'slug', 'parent_id'];
+
+    public function attributes()
+    {
+        return $this->hasMany(Attribute::class, 'category_id', 'id');
+    }
+
+    public function parentAttributes(): array
+    {
+        return $this->parent ? $this->parent->allAttributes() : [];
+    }
+
+    public function allAttributes(): array
+    {
+        return array_merge($this->parentAttributes(), $this->attributes()->orderBy('sort')->getModels());
+    }
+
 }
